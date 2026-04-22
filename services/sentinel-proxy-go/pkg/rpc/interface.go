810b8cdad8916283e4bc6dc39be64db08d6c81ec
mrheed <syahidnurrohim@gmail.com>
2026-04-22 20:51:22 +0700

    clean: remove sentinel-proxy-go

--- services/sentinel-proxy-go/pkg/rpc/interface.go
+++
@@ -1,9 +0,0 @@
-package rpc
-
-import "context"
-
-type RPCClient interface {
-	IsReady(ctx context.Context) (bool, error)
-	GetBlockNumber(ctx context.Context) (int, error)
-	GetValidatorsStats(ctx context.Context) (*GetValidatorsStatsResponse, error)
-}
